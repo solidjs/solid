93c4b6dc38b9cc7b6576a251787df58c6a4e83cb
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-08-24 14:40:34 +0200

    draft for type tests

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,23 @@
 module.exports = {
   env: {
     test: {
-      presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-      plugins: [['babel-plugin-jsx-dom-expressions', {moduleName: '../../dist/dom/index.js', contextToCustomElements: true, builtIns: ['For', 'Show', 'Switch', 'Match', 'Suspense', 'Portal']}]]
+      //presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+      //plugins: [['babel-plugin-jsx-dom-expressions', {moduleName: '../../dist/dom/index.js', contextToCustomElements: true, builtIns: ['For', 'Show', 'Switch', 'Match', 'Suspense', 'Portal']}]]
+      "presets": [
+        ['@babel/preset-env', {targets: {node: 'current'}}],
+        "@babel/preset-typescript",
+      ],
+      "plugins": [
+        [
+          "babel-plugin-jsx-dom-expressions",
+          {
+            "moduleName": "../../dist/dom/index.js",
+            contextToCustomElements: true,
+            builtIns: ['For', 'Show', 'Switch', 'Match', 'Suspense', 'Portal']
+          }
+        ],
+      ]
+
     }
   }
 };
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   collectCoverageFrom: [
     'dist/index.js',
     'dist/dom/index.js'
-  ]
+  ],
+  //preset: 'ts-jest/presets/js-with-babel',
+  //"transform": {
+  //  "^.+\\.tsx?$": "ts-jest"
+  //},
 }
