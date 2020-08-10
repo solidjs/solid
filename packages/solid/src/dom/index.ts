e5ce004bac94df32cefe7c7f2ddba71a35f7575a
Dmitriy Nikifrov <tuningiposadka@gmail.com>
2020-08-10 20:09:29 +0500

    add containerNeeded flag

--- packages/solid/src/dom/index.ts
+++ packages/solid/src/dom/index.ts
@@ -4,15 +4,12 @@ import { onCleanup, sample, splitProps, Component } from "../index.js";
 export * from "./runtime";
 export { For, Match, Show, Suspense, SuspenseList, Switch, Index } from "../index.js";
 
-export function Portal(props: {
-  mount?: Node;
-  useShadow?: boolean;
-  children: JSX.Element;
-}) {
+export function Portal(props: { mount?: HTMLElement; useShadow?: boolean; children: JSX.Element }) {
+  const containerNeeded = !props.mount || typeof props.children === "string";
   const { useShadow } = props,
-    container = props.mount ?? document.createElement("div"),
+    container = containerNeeded ? document.createElement("div") : props.mount!,
     marker = document.createTextNode(""),
-    mount = props.mount ?? document.body,
+    mount = props.mount || document.body,
     renderRoot =
       useShadow && container.attachShadow ? container.attachShadow({ mode: "open" }) : container;
 
@@ -25,9 +22,9 @@ export function Portal(props: {
     renderRoot,
     sample(() => props.children)
   );
-  mount.appendChild(container);
+  if (containerNeeded) mount.appendChild(container);
   (props as any).ref && (props as any).ref(container);
-  onCleanup(() => mount.removeChild(container));
+  onCleanup(() => containerNeeded && mount.removeChild(container));
   return marker;
 }
 
