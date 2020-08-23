4d6427de77121b579818561efba99d934aabfc59
Ryan Carniato <ryansolid@gmail.com>
2020-08-22 22:45:22 -0700

    remove dom expressions typescript dep on hyper/lit

--- packages/solid/src/dom/h.ts
+++ packages/solid/src/dom/h.ts
@@ -1,11 +1,10 @@
 import { createHyperScript } from "hyper-dom-expressions";
-import { spread, assign, insert, createComponent, delegateEvents, dynamicProperty } from "./index.js";
+import { spread, assign, insert, createComponent, dynamicProperty } from "./index.js";
 
 export default createHyperScript({
   spread,
   assign,
   insert,
   createComponent,
-  delegateEvents,
   dynamicProperty
 });
