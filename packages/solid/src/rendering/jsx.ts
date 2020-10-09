41ee206e9ed383f0d5bdbaf881fdf9ce3a71bac1
gvatn <gudmund.vatn@gmail.com>
2020-10-09 21:04:49 +0200

    Inputmode on HTMLAttributes

--- packages/solid/src/rendering/jsx.ts
+++ packages/solid/src/rendering/jsx.ts
@@ -1962,6 +1962,7 @@ declare global {
       align?: "start" | "end" | "center" | "baseline" | "stretch" | "left" | "right";
       part?: string;
       exportparts?: string;
+      inputMode?: 'none' | 'text' | 'tel' | 'url' | 'email' | 'numeric' | 'decimal' | 'search';
     }
 
     // HTML Elements
