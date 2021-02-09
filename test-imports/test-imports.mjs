dbf42845791f2df750105c23267892c25529f46d
Joe Pea <joe@trusktr.io>
2021-02-09 13:37:15 -0800

    add tests for ensuring we follow Node ES Module rules correctly
    Major tools besides Node follow the new ESM standards set by Node (f.e.
    Webpack 5 follows the standards by default out of the box). This test
    ensures solid-js is compatible with consumers that will import from
    aliases, as well as with consumers that need to import from particular
    files directly.

---
+++ test-imports/test-imports.mjs
@@ -0,0 +1,42 @@
+function checkError(error) {
+  // This error happens when missing the type:module field in package.json when it is needed.
+  if (
+    error instanceof SyntaxError &&
+    error.message.includes("Cannot use import statement outside a module")
+  ) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // These errors happen if exports are not mapped to files that should be importable.
+  if (["ERR_PACKAGE_PATH_NOT_EXPORTED", "ERR_MODULE_NOT_FOUND"].includes(error.code)) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Any other errors (unless I missed any that should be added to the checks
+  // above) are errors that happen after imported modules are successfully
+  // resolved (f.e. a module was found, but it doesn't export a particular
+  // identifier when running in node vs browser). We don't want to fail the
+  // test in those cases, because we're testing only that ESM exports are set
+  // up correctly. Importing `solid-js/h` will fail in node even if modules are
+  // resolved properly, for example.
+}
+
+await Promise.all([
+  import("solid-js/dev").catch(checkError),
+  import("solid-js/dev/dist/dev.js").catch(checkError),
+
+  import("solid-js/web").catch(checkError),
+  import("solid-js/web/dist/web.js").catch(checkError),
+  import("solid-js/web/dist/server.js").catch(checkError),
+  import("solid-js/web/dist/server-async.js").catch(checkError),
+
+  import("solid-js/h").catch(checkError),
+  import("solid-js/h/dist/h.js").catch(checkError),
+
+  import("solid-js/html").catch(checkError),
+  import("solid-js/html/dist/html.js").catch(checkError)
+]);
+
+console.log("ES Module import test passed.");
