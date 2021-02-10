e21e1d2fd3b580a6b3074667feec0078d2bb37a2
Joe Pea <joe@trusktr.io>
2021-02-09 19:26:21 -0800

    apparently the package.json:types field is needed for places with nested package.json files… (#346)
    * apparently the package.json:types field is needed for places with nested package.json files, but only the exports field should be at the top level of the published package
    
    * add tests for ensuring we follow Node ES Module rules correctly
    
    Major tools besides Node follow the new ESM standards set by Node (f.e.
    Webpack 5 follows the standards by default out of the box). This test
    ensures solid-js is compatible with consumers that will import from
    aliases, as well as with consumers that need to import from particular
    files directly.
    
    * make test-imports private:true instead of ignoring it in the lerna publish command
    
    * moved test-imports into packages folder
    
    * ensure to run tests in CI too, so pull requests are tested
    
    * don't use top-level await so that test-imports works in older versions of Node like in CI
    
    * update Travis CI to Node 14
    
    * delete file from test-imports which was an artefact from a previous version of solid
    
    * make sure all packages have a clean script so no artefacts are left behind

---
+++ packages/test-imports/test-imports.mjs
@@ -0,0 +1,47 @@
+function checkError(error) {
+  // This error happens when missing the type:module field in package.json when it is needed.
+  if (
+    error instanceof SyntaxError &&
+    error.message.includes("Cannot use import statement outside a module")
+  ) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // These errors happen if exports are not mapped to files that should be importable.
+  if (["ERR_PACKAGE_PATH_NOT_EXPORTED", "ERR_MODULE_NOT_FOUND"].includes(error.code)) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // Any other errors (unless I missed any that should be added to the checks
+  // above) are errors that happen after imported modules are successfully
+  // resolved (f.e. a module was found, but it doesn't export a particular
+  // identifier when running in node vs browser).  SO we silence them by not
+  // re-throwing them here as we don't want to fail the test in those cases,
+  // because we're testing only that ESM exports are set up correctly.
+  // Importing `solid-js/h` will fail in node even if modules are resolved
+  // properly, for example.
+}
+
+Promise.all([
+  import("solid-js/dev").catch(checkError),
+  import("solid-js/dev/dist/dev.js").catch(checkError),
+
+  import("solid-js/web").catch(checkError),
+  import("solid-js/web/dist/web.js").catch(checkError),
+  import("solid-js/web/dist/server.js").catch(checkError),
+
+  import("solid-js/h").catch(checkError),
+  import("solid-js/h/dist/h.js").catch(checkError),
+
+  import("solid-js/html").catch(checkError),
+  import("solid-js/html/dist/html.js").catch(checkError)
+])
+  .then(() => {
+    console.log("ES Module import test passed.");
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
