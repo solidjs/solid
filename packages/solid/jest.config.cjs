4803e6dc07b1dd782565ecb956a7fc0dfd565905
Ryan Carniato <ryansolid@gmail.com>
2021-06-22 23:59:50 -0700

    update coverage

--- packages/solid/jest.config.cjs
+++ packages/solid/jest.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
   verbose: false,
-  collectCoverageFrom: ["src/**/*.ts", "web/src/**/*.ts", "!**/*.d.ts", "!src/server/*.ts"],
+  collectCoverageFrom: ["src/**/*.ts", "store/src/**/*.ts", "web/src/**/*.ts", "!**/*.d.ts", "!src/server/*.ts", "!store/src/**/server.ts"],
   transformIgnorePatterns: ["node_modules/?!(dom-expressions)"]
 };
