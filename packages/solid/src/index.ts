f26688a04a5b71928bc7717dd9ce34f192cda3f3
Joe Pea <joe@trusktr.io>
2022-03-31 19:12:46 -0700

    re-export all things from signal.ts (they are available indirectly via the path to the file anyway)

--- packages/solid/src/index.ts
+++ packages/solid/src/index.ts
@@ -1,47 +1,4 @@
-export {
-  createRoot,
-  createSignal,
-  createEffect,
-  createRenderEffect,
-  createComputed,
-  createReaction,
-  createDeferred,
-  createSelector,
-  createMemo,
-  createResource,
-  onMount,
-  onCleanup,
-  onError,
-  untrack,
-  batch,
-  on,
-  enableScheduling,
-  enableExternalSource,
-  startTransition,
-  useTransition,
-  refetchResources,
-  createContext,
-  useContext,
-  children,
-  getListener,
-  getOwner,
-  runWithOwner,
-  equalFn,
-  $DEVCOMP,
-  $PROXY
-} from "./reactive/signal";
-export type {
-  Accessor,
-  Setter,
-  Signal,
-  Resource,
-  ResourceReturn,
-  ResourceFetcher,
-  ResourceFetcherInfo,
-  Context,
-  ReturnTypes
-} from "./reactive/signal";
-
+export * from "./reactive/signal";
 export * from "./reactive/observable";
 export * from "./reactive/scheduler";
 export * from "./reactive/array";
