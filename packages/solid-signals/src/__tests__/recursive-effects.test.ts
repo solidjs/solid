e7cccc42b477b771e334428a6be0cb32690cdb8f
Tanner Linsley <tannerlinsley@gmail.com>
2023-01-09 12:23:27 -0700

    Update recursive-effects.test.ts

--- packages/solid-signals/src/__tests__/recursive-effects.test.ts
+++ packages/solid-signals/src/__tests__/recursive-effects.test.ts
@@ -1,98 +1,98 @@
-import { describe, it, expect, test } from 'vitest'
+import { describe, it, expect, test } from "vitest";
 
-import { createEffect, createRoot, untrack } from '../core'
-import { createStore, unwrap } from '../store'
-import { sharedClone } from './sharedClone'
+import { createEffect, createRoot, untrack } from "../core";
+import { createStore, unwrap } from "../store";
+import { sharedClone } from "./sharedClone";
 
-describe('recursive effects', () => {
-  it('can track deeply with cloning', () => {
-    const [store, setStore] = createStore({ foo: 'foo', bar: { baz: 'baz' } })
+describe("recursive effects", () => {
+  it("can track deeply with cloning", () => {
+    const [store, setStore] = createStore({ foo: "foo", bar: { baz: "baz" } });
 
-    let called = 0
-    let next: any
-
-    createRoot(() => {
-      createEffect(() => {
-        next = sharedClone(next, store)
-        called++
-      })
-    })
-
-    setStore((s) => {
-      s.foo = '1'
-    })
-
-    setStore((s) => {
-      s.bar.baz = '2'
-    })
-
-    expect(called).toBe(3)
-  })
-
-  it('respects untracked', () => {
-    const [store, setStore] = createStore({ foo: 'foo', bar: { baz: 'baz' } })
-
-    let called = 0
-    let next: any
+    let called = 0;
+    let next: any;
 
     createRoot(() => {
       createEffect(() => {
-        next = sharedClone(next, untrack(() => store).bar)
-        called++
-      })
-    })
+        next = sharedClone(next, store);
+        called++;
+      });
+    });
 
     setStore((s) => {
-      s.foo = '1'
-    })
+      s.foo = "1";
+    });
 
     setStore((s) => {
-      s.bar.baz = '2'
-    })
+      s.bar.baz = "2";
+    });
+
+    expect(called).toBe(3);
+  });
+
+  it("respects untracked", () => {
+    const [store, setStore] = createStore({ foo: "foo", bar: { baz: "baz" } });
+
+    let called = 0;
+    let next: any;
+
+    createRoot(() => {
+      createEffect(() => {
+        next = sharedClone(next, untrack(() => store).bar);
+        called++;
+      });
+    });
+
+    setStore((s) => {
+      s.foo = "1";
+    });
+
+    setStore((s) => {
+      s.bar.baz = "2";
+    });
 
     setStore((s) => {
       s.bar = {
-        baz: '3',
-      }
-    })
+        baz: "3",
+      };
+    });
 
-    expect(called).toBe(3)
-  })
+    expect(called).toBe(3);
+  });
 
-  it('supports unwrapped values', () => {
-    const [store, setStore] = createStore({ foo: 'foo', bar: { baz: 'baz' } })
+  it("supports unwrapped values", () => {
+    const [store, setStore] = createStore({ foo: "foo", bar: { baz: "baz" } });
 
-    let called = 0
-    let prev: any
-    let next: any
+    let called = 0;
+    let prev: any;
+    let next: any;
 
     createRoot(() => {
       createEffect(() => {
-        prev = next
-        next = unwrap(sharedClone(next, store))
-        called++
-      })
-    })
+        prev = next;
+        next = unwrap(sharedClone(next, store));
+        called++;
+      });
+    });
 
     setStore((s) => {
-      s.foo = '1'
-    })
+      s.foo = "1";
+    });
 
     setStore((s) => {
-      s.bar.baz = '2'
-    })
+      s.bar.baz = "2";
+    });
 
-    expect(next).not.toBe(prev)
-    expect(called).toBe(3)
-  })
+    expect(next).not.toBe(prev);
+    expect(called).toBe(3);
+  });
 
-  it('supports getters that return frozen objects', () => {
+  it("supports getters that return frozen objects", () => {
     const [store, setStore] = createStore({
       get foo() {
-        return Object.freeze({ foo: 'foo' })
+        return Object.freeze({ foo: "foo" });
       },
-    })
+    });
 
-    expect(() => store.foo).not.toThrow()
-  })
-})
+    expect(() => store.foo).not.toThrow();
+  });
+});
