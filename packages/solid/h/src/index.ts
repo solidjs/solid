f47eaa41f6205dda649fdf3b973ceeff8948a59c
lorefnon <lorefnon@gmail.com>
2023-12-28 14:40:36 +0530

    Make hyperscript API more type safe
    Ensure that invalid props for native elements, solid components and
    custo elements result in type errors.

--- packages/solid/h/src/index.ts
+++ packages/solid/h/src/index.ts
@@ -1,5 +1,4 @@
 import { createHyperScript } from "./hyperscript";
-import type { HyperScript } from "./hyperscript";
 import {
   spread,
   assign,
@@ -8,14 +7,51 @@ import {
   dynamicProperty,
   SVGElements
 } from "solid-js/web";
+import type { Component, JSX } from "solid-js";
 
-const h: HyperScript = createHyperScript({
+export type MaybeFn<T> = T | (() => T);
+export type MaybeArr<T> = T | T[];
+export type MaybeSelector<T extends string> = T | `${T}#${string}` | `${T}.${string}`;
+
+export type MaybeFnProps<T extends object> = {
+  [K in keyof T]: MaybeFn<T[K]>;
+};
+
+export interface SolidHyperScript {
+  <K extends keyof JSX.IntrinsicElements, P extends MaybeFnProps<JSX.IntrinsicElements[K]>>(
+    key: MaybeSelector<K>,
+    props: MaybeFnProps<P>,
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  <K extends keyof JSX.IntrinsicElements, P extends MaybeFnProps<JSX.IntrinsicElements[K]>>(
+    key: {} extends JSX.IntrinsicElements[K] ? MaybeSelector<K> : never, // Prevent skipping args if mandatory props present
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  <P extends object>(
+    component: Component<P>,
+    props: MaybeFnProps<P>,
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  <P extends object>(
+    component: {} extends P ? Component<P> : never, // Prevent skipping args if mandatory props present
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  Fragment: (props: {
+    children: (() => JSX.Element) | (() => JSX.Element)[];
+  }) => MaybeArr<JSX.Element>;
+}
+
+const h = createHyperScript({
   spread,
   assign,
   insert,
   createComponent,
   dynamicProperty,
   SVGElements
-});
+}) as SolidHyperScript;
 
 export default h;
