d5466e6be44b7c7fbefe75a78e98969b1abaa7cb
lorefnon <lorefnon@gmail.com>
2023-12-28 19:49:03 +0530

    Add special case for div being implied when selector is not accompanied by tag

--- packages/solid/h/src/index.ts
+++ packages/solid/h/src/index.ts
@@ -11,7 +11,9 @@ import type { Component, JSX } from "solid-js";
 
 export type MaybeFn<T> = T | (() => T);
 export type MaybeArr<T> = T | T[];
-export type MaybeSelector<T extends string> = T | `${T}#${string}` | `${T}.${string}`;
+export type Selector<T extends string> = `${T}#${string}` | `${T}.${string}`;
+export type BareSelector = Selector<"">
+export type MaybeSelector<T extends string> = T | Selector<T>;
 
 export type MaybeFnProps<T extends object> = {
   [K in keyof T]: MaybeFn<T[K]>;
@@ -26,6 +28,17 @@ export interface SolidHyperScript {
 
   <K extends keyof JSX.IntrinsicElements, P extends MaybeFnProps<JSX.IntrinsicElements[K]>>(
     key: {} extends JSX.IntrinsicElements[K] ? MaybeSelector<K> : never, // Prevent skipping args if mandatory props present
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  <P extends MaybeFnProps<JSX.IntrinsicElements["div"]>>(
+    key: BareSelector,
+    props: MaybeFnProps<P>,
+    ...children: MaybeArr<MaybeFn<JSX.Element>>[]
+  ): () => MaybeArr<JSX.Element>;
+
+  <P extends MaybeFnProps<JSX.IntrinsicElements["div"]>>(
+    key: BareSelector,
     ...children: MaybeArr<MaybeFn<JSX.Element>>[]
   ): () => MaybeArr<JSX.Element>;
 
