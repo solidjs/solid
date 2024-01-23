d6b9418d851b73f893e3edfe495bf2073d86b354
Ryan Carniato <ryansolid@gmail.com>
2024-01-22 16:10:16 -0800

    ensure inertness in test for createMutable

--- packages/solid/store/test/mutableWithClass.spec.tsx
+++ packages/solid/store/test/mutableWithClass.spec.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from "../../src";
+import { createEffect, createRoot } from "../../src";
 import { createMutable } from "../src";
 
 describe("Class Operator test", () => {
@@ -17,6 +17,8 @@ describe("Class Operator test", () => {
       child = new D();
     }
     let m: any;
+    let count = 0,
+      childCount = 0;
     const increment = () => {
       m.a++;
       m.child.f++;
@@ -24,16 +26,30 @@ describe("Class Operator test", () => {
 
     createRoot(() => {
       m = createMutable(new A());
+      createEffect(() => {
+        m.b;
+        count++;
+      });
+      createEffect(() => {
+        m.child.f;
+        childCount++;
+      });
       increment();
     });
 
     expect(m.b).toBe(8);
     expect(m.child.e).toBe(8);
+    expect(count).toBe(1);
+    expect(childCount).toBe(1);
     increment();
     expect(m.b).toBe(12);
     expect(m.child.e).toBe(12);
+    expect(count).toBe(2);
+    expect(childCount).toBe(1);
     increment();
     expect(m.b).toBe(16);
     expect(m.child.e).toBe(16);
+    expect(count).toBe(3);
+    expect(childCount).toBe(1);
   });
 });
