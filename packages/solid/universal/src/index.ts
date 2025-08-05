a0207d2217f1bb10820c036eb8d297289f55fbbb
Damian Tarnawski <gthetarnav@gmail.com>
2025-08-05 19:24:31 +0200

    Export RendererOptions and Renderer types from solid-js/universal

--- packages/solid/universal/src/index.ts
+++ packages/solid/universal/src/index.ts
@@ -2,6 +2,8 @@ import { createRenderer as createRendererDX } from "./universal.js";
 import type { RendererOptions, Renderer } from "./universal.js";
 import { mergeProps } from "solid-js";
 
+export type { RendererOptions, Renderer } from "./universal.js";
+
 export function createRenderer<NodeType>(options: RendererOptions<NodeType>): Renderer<NodeType> {
   const renderer = createRendererDX(options);
   renderer.mergeProps = mergeProps;
