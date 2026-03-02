7c37cbb01314164b5249ed9e476b0d6938c9a376
Brenley Dueck <brenleydueck@gmail.com>
2026-03-02 11:36:26 -0600

    Issue with spread

--- examples/ssr/shared/src/components/Home.js
+++ examples/ssr/shared/src/components/Home.js
@@ -1,17 +1,28 @@
 import { createSignal, Errored, Loading, onSettled } from "solid-js";
 const Home = () => {
   const [count, setCount] = createSignal(0);
+  const props = {
+    test: "test",
+    test2: "test2"
+  };
   return (
     <div>
-      <div>{count()}</div>
-      <Show when={true}>
-        <button onClick={() => setCount(count() + 1)}>Click me in first child</button>
-      </Show>
-      <Show when={true}>
-        <button onClick={() => setCount(count() + 1)}>Click me in second child</button>
-      </Show>
+      <Link count={1} />
+      <Link count={2} />
     </div>
   );
 };
+
+function Link(props) {
+  const linkProps = {
+    href: "/",
+    onClick: e => {
+      e.preventDefault();
+      console.log("clicked", props.count);
+    }
+  };
+
+  return <a {...linkProps}>My Link {props.count}</a>;
+}
 
 export default Home;
